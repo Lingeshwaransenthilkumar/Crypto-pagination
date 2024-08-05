514b12ba0f7d1734f1c7f9e9bc5b5bed68d875f0
Lingeshwaransenthilkumar <lingeshwaransenthilkumar0279@gmail.com>
2024-08-05 14:19:17 +0530

    base chanegd

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:["/pagination-2/"]
+  base:["/pagination-second/"]
 })
